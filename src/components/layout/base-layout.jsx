07c7de752292b9994bff919acdd6b04e8236083f
JuanHermo <juanite.dev@gmail.com>
2025-04-01 07:12:10 +0200

    New logo

--- src/components/layout/base-layout.jsx
+++ src/components/layout/base-layout.jsx
@@ -4,7 +4,7 @@ export default function BaseLayout({ children }) {
         <div className="bg-gray-100 flex justify-center items-center min-h-screen">
           <div className="w-1/2 h-screen hidden lg:block bg-[#3a9932] overflow-hidden">
             <img
-              src="/logo_login_new.png"
+              src="/logo_login_new_2.png"
               alt="Placeholder"
               className="object-cover w-full h-full animate-zoom"
             />
